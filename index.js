1674ddd4a40ea42c0a73e676c6a9cc9cee7ea258
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-09-21 23:57:46 -0700

    commit tree fdb2f3160ed849fed5560a9584f7e86a5b650cd6

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Shawnte'
+var bestCustomer
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(name){
+    bestCustomer = name
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Not Shawnte'
+    return leastFavoriteCustomer
+}
